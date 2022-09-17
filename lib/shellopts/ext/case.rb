c831820f43780b22aa57ab68385b69c479296a62
Claus Rasmussen <claus.l.rasmussen@gmail.com>
2022-09-17 16:32:23 +0200

    Added case extension

---
+++ lib/shellopts/ext/case.rb
@@ -0,0 +1,20 @@
+
+module CaseMatcher
+  class CaseMatcher < BasicObject
+  private
+    def self.new(*args) = raise "Can't create #{self.class} objects"
+  end
+
+  class Ordinal < CaseMatcher
+    def self.===(other) = other.is_a?(::Integer) && other >= 1 || super
+  end
+
+  class Empty < CaseMatcher
+    def self.===(other) = other.respond_to?(:empty?) && other.empty? || super
+    def self.! = ::CaseMatcher::NotEmpty
+  end
+
+  class NotEmpty < CaseMatcher
+    def self.===(other) = other.respond_to?(:empty?) && !other.empty? || super
+  end
+end
